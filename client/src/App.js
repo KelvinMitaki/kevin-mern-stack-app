e53588678710361788591ac6994deeed1e4026bc
KelvinMitaki <mitakikelvin1@gmail.com>
2019-12-28 20:00:34 +0300

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import store from "./store";
 import ItemModal from "./components/ItemModal";
 import { loadUser } from "./actions/authAction";
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export class App extends Component {
   componentDidMount() {
@@ -16,14 +17,16 @@ export class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        {" "}
-        <div className="App">
-          <AppNavbar />
-          <Container>
-            <ItemModal />
-            <ShoppingList />
-          </Container>
-        </div>
+        <Router>
+          <div className="App">
+            <AppNavbar />
+            <Container>
+              {" "}
+              <Route exact path="/" component={ItemModal} />
+              <ShoppingList />
+            </Container>
+          </div>
+        </Router>
       </Provider>
     );
   }
